5af51e24e76a42b2824266bf2c907107fa669e4b
Lauren Pyfer <laurenpyfer@Laurens-MacBook-Air-54.local>
2022-11-16 13:57:22 -0500

    commit

--- discussion11.py
+++ discussion11.py
@@ -2,6 +2,7 @@ import unittest
 import sqlite3
 import json
 import os
+#Lauren Pyfer lpyfer@umich.edu
 # starter code
 
 # Create Database
@@ -10,7 +11,7 @@ def setUpDatabase(db_name):
     conn = sqlite3.connect(path+'/'+db_name)
     cur = conn.cursor()
     return cur, conn
-
+pass
 
 # Creates list of species ID's and numbers
 def create_species_table(cur, conn):
@@ -39,12 +40,15 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
-    pass
-
+   cur.execute("DROP TABLE IF EXISTS Patients")
+   cur.execute("CREATE TABLE \"Patients\"(\"pet_id\" INTEGER PRIMARY KEY, \"name\" TEXT, \"species_id\"  NUMBER, \"age\" INETGER, \"cuteness\" INTEGER, \"aggressiveness\" NUMBER)")
+   conn.commit()
 
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
-    pass
+    cur.execute("INSERT INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness) VALUES(?, ?, ?, ?, ?, ?)", (0, 'Fluffle', 'Rabbit', 0, 3, 90, 100))
+    conn.commit()
+
     
 
 # TASK 2
@@ -59,14 +63,30 @@ def add_pets_from_json(filename, cur, conn):
     json_data = json.loads(file_data)
 
     # THE REST IS UP TO YOU
-    pass
+    pet_id = 1
+    for item in json_data:
+        name = item['name']
+        species = item['species']
+        age = int(item['age'])
+        cuteness = int(item['cuteness'])
+        aggressiveness = int(item['aggressiveness'])
+        
+        cur.execute('SELECT id from Species WHERE title = ?', (species,)) #(species,) is a tuple
+        species_id = int(cur.fetchone()[0])
 
+        cur.execute("INSERT INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness) VALUES(?,?,?,?,?,?)",(pet_id, name, species_id, age, cuteness, aggressiveness)) #use question mark
+
+        pet_id += 1
 
 # TASK 3
 # CODE TO OUTPUT NON-AGGRESSIVE PETS
 def non_aggressive_pets(aggressiveness, cur, conn):
-    pass
-
+    cur.execute("SELECT name FROM Patients WHERE aggressiveness <= ?", (aggressiveness,))
+    rows = cur.fetchall()
+    non_agg_ls = []
+    for row in rows:
+        non_agg_ls.append(row[0])
+    return non_agg_ls
 
 
 def main():
